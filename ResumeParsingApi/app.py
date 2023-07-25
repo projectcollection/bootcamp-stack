e16ada23c85d881287049609eb12026375ee4617
SNVA0011 <96859907+SNVA0011@users.noreply.github.com>
2023-07-25 16:39:50 -0400

    Added the resume parser

---
+++ ResumeParsingApi/app.py
@@ -0,0 +1,54 @@
+import os
+import urllib.request
+from flask import Flask, request, redirect, jsonify
+from werkzeug.utils import secure_filename
+from resume_parser import resumeparse
+import json
+
+UPLOAD_FOLDER = './uploads'
+RESULTS_FOLDER = './results'
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+ALLOWED_EXTENSIONS = set(['doc', 'pdf', 'docx'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/api/v1/file-upload', methods=['POST'])
+def upload_file():
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		resp = jsonify({'message' : 'No file part in the request'})
+		resp.status_code = 400
+		return resp
+	file = request.files['file']
+	if file.filename == '':
+		resp = jsonify({'message' : 'No file selected for uploading'})
+		resp.status_code = 400
+		return resp
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)        
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		data=resumeparse.read_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))		
+		result = json.dumps(data)
+		print(type(data))
+		print(data)
+		print(result)
+		resp = jsonify({'message' : 'File successfully uploaded','status_code': 201,'data': result})		
+		resp.status_code = 201
+		return resp
+	else:
+		resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+		resp.status_code = 400
+		return resp
+
+
+
+
+if __name__ == "__main__":
+    app.run()
